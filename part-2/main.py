a9d9a038b58dd3aef98476b03a0fe1122bf06282
Aniruddha Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2022-07-30 12:49:39 +0530

    solution for part 2, along with docs

--- part-2/main.py
+++ part-2/main.py
@@ -1,16 +1,26 @@
 import doctest
 
 
-def _brute_force_is_ascending(num: int) -> bool:
+def is_ascending(num: int | list[int]) -> bool:
     '''
-    >>> _brute_force_is_ascending(12345)
-    True 
-    >>> _brute_force_is_ascending(111110)
+    >>> is_ascending(12345)
+    True
+    >>> is_ascending(589)
+    True
+    >>> is_ascending(111110)
     False
-    >>> brute_force_is_ascending(212)
+    >>> is_ascending(212)
+    False
+    >>> is_ascending([1,1,1,1,1,0])
     False
     '''
-    digits = [int(d) for d in str(num)]
+    digits: list[int]
+    if isinstance(num, int):
+        digits = [int(d) for d in str(num)]
+    elif isinstance(num, list):
+        digits = num
+    else:
+        raise TypeError('num should be a list or int')
     for i in range(1, len(digits)):
         if digits[i-1] > digits[i]:
             return False
@@ -24,16 +34,65 @@ def brute_force_find_ascending_num(num: int) -> int:
     if num < 0:
         raise ValueError('Expected a positive number')
     for val in range(num, 0, -1):
-        if _brute_force_is_ascending(val):
+        if is_ascending(val):
             return val 
     return 0
 
+
+def flip_leftmost_mismatch(digits: list[int]):
+    '''
+    >>> flip_leftmost_mismatch([3, 2, 4, 9, 1])
+    [2, 9, 9, 9, 9]
+    >>> flip_leftmost_mismatch([1, 0])
+    [0, 9]
+    '''
+    n = len(digits)
+    for i in range(0, n-1):
+        if digits[i] > digits[i+1]:
+            digits[i] -= 1
+            for j in range(i+1, n):
+                digits[j] = 9
+    return digits
+
+
 def find_ascending_num(num: int) -> int:
-    return 0
+    '''
+    see tests.py for more unit tests
+
+    idea - keep flipping leftmost mismatches till you get an ascending number
+
+    eg. 23245
+
+        23245
+        ^ mismatch for indexes (1,2) since 3 > 2 
+        so flip 32 into 29, and the rest of the following digits are also 9
+        22999
+          done
+
+    eg. 33245
+
+        33245
+          ^ mismatch at (1, 2) for '32'
+        32999
+          ^ mismatch at (0, 1) for '32'
+        29999
+          done 
+     
+    input is number num
+    
+    N := log_10(num) ie. num of decimal digits
+
+    :: Time complexity 
+    worst cast e.g. for 11110 -> O(N^2)
+
+    :: Space complexity
+    O(N), for storing digits array
+    '''
+    digits = [int(d) for d in str(num)]
+    while not is_ascending(digits):
+        digits = flip_leftmost_mismatch(digits[:])
+    result = int(''.join((str(d) for d in digits)))
+    return result
 
 if __name__ == '__main__':
-    #doctest.testmod()
-    print("num,output")
-    for num in range(0, 9999):
-        res = brute_force_find_ascending_num(num)
-        print(f'{num},{res}')
+    doctest.testmod()
--- part-2/tests.py
+++ part-2/tests.py
@@ -7,20 +7,27 @@ class Tests(unittest.TestCase):
     def setUp(self):
         self.test_cases = (
             # (num, exepected_output)
+            (9, 9),
+            (10,9),
+            (30,29),
+            (218,199),
+            (389,389),
+            (5552, 4999),
+            (9904,8999),
             (23245, 22999),
             (11235888, 11235888),
             (111110, 99999),
             (33245, 29999),
-            (9, 9),
-            (0, 0),
+            (1916269294, 1899999999)
         )
 
-    @unittest.skip("not implemented")
+   
     def test_find_ascending_nums(self):
         for num, exepected_output in self.test_cases:
             output = find_ascending_num(num)
             self.assertEqual(output, exepected_output)
     
+    @unittest.skip
     def test_find_ascending_nums_brute_force(self):
         for num, exepected_output in self.test_cases:
             output = brute_force_find_ascending_num(num)
